5eaf62aace471de3e8e938cf0ade759ad495390e
choongangkangnam2 <choongangkangnam2@choongang.com>
2020-01-22 09:24:02 +0900

    request parameter multi vaule

---
+++ src/chapter17/FormEx3Servlet.java
@@ -0,0 +1,70 @@
+package chapter17;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FormEx3Servlet
+ */
+@WebServlet("/formex3")
+public class FormEx3Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FormEx3Servlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html; charset=utf-8");
+		
+		PrintWriter out = response.getWriter();
+		
+		out.println("<ul>");
+		
+		String value1 = request.getParameter("number1");
+		String value2 = request.getParameter("number2");
+		
+		out.println("number1=" + value1);
+		out.println("number2=" + value2);
+		
+		out.println("</ul>");
+		
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
