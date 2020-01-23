c28d2c3842c6ea6b155b91719ef2f717c4f80020
choongangkangnam2 <choongangkangnam2@choongang.com>
2020-01-23 11:16:27 +0900

    type parameter

---
+++ src/chapter03/ParameterEx1Servlet.java
@@ -0,0 +1,81 @@
+package chapter03;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+import java.util.Map;
+import java.util.Set;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ParameterEx1Servlet
+ */
+@WebServlet("/chapter03/viewParameter")
+public class ParameterEx1Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ParameterEx1Servlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter out = response.getWriter();
+		
+		out.print("<ul>");
+		Enumeration names = request.getParameterNames();
+		while (names.hasMoreElements()) {
+			String name = (String) names.nextElement();
+			out.print("<li>" + name + "</li>");
+		}
+		out.print("<ul>");
+		
+		Map map = request.getParameterMap();
+		Set keys = map.keySet();
+		
+		for (Object key : keys) {
+			String[] values = (String[]) map.get(key);
+			out.print("<h3>" + key + "</h3>");
+			if (values != null) {
+				for (String value : values) {
+					out.print(value);
+					out.print("<br>");
+				}
+			}
+		}
+		
+		
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
