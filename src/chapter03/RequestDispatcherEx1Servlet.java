5d0cdcf1fe9b4e77cac94ee4126b5800024bf6b1
choongangkangnam2 <choongangkangnam2@choongang.com>
2020-01-23 15:33:20 +0900

    request dispatcher

---
+++ src/chapter03/RequestDispatcherEx1Servlet.java
@@ -0,0 +1,43 @@
+package chapter03;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RequestDispatcherEx1Servlet
+ */
+@WebServlet("/chapter03/reqdis")
+public class RequestDispatcherEx1Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RequestDispatcherEx1Servlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher dest = request.getRequestDispatcher("/chapter03/game1.jsp");
+		dest.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
