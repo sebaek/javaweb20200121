ca7465ec18bcad61098b4695b46d15aef4a23b41
choongangkangnam2 <choongangkangnam2@choongang.com>
2020-01-30 14:15:36 +0900

    java bean

---
+++ src/chapter08/MyBean.java
@@ -0,0 +1,26 @@
+package chapter08;
+
+public class MyBean {
+	private String name;
+	private int age;
+	private boolean married;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public boolean isMarried() {
+		return married;
+	}
+	public void setMarried(boolean married) {
+		this.married = married;
+	}
+}
