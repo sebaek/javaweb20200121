b4b49bbda3f6d8f21d2630e000a63d32e94a1cba
choongangkangnam2 <choongangkangnam2@choongang.com>
2020-02-06 16:11:34 +0900

    view post

--- WebContent/WEB-INF/chapter16/view.jsp
+++ WebContent/WEB-INF/chapter16/view.jsp
@@ -6,24 +6,18 @@
 <head>
 <meta charset="UTF-8">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-<title>게시판 목록 보기</title>
+<title>게시물 보기</title>
 </head>
 <body>
-<h1>게시판 목록 보기</h1>
-<table>
-<tr><th>번호</th><th>제목</th></tr>
-<c:forEach var="post" items="${board }">
-<tr>
-<td>${post.id }</td>
-<td><a href="view?id=${post.id }"><c:out value="${post.title }" /></a></td>
-</tr>
-</c:forEach>
+<h1>게시물 보기</h1>
 
-</table>
+<h3>[${post.id}] <c:out value="${post.title }" /></h3>
+<textarea rows="5" cols="25" readonly>
+<c:out value="${post.body }" />
+</textarea>
 
 </body>
 </html>
-
 
 
 
---
+++ src/chapter16/ViewServlet.java
@@ -0,0 +1,58 @@
+package chapter16;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ViewServlet
+ */
+@WebServlet("/chapter16/view")
+public class ViewServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ViewServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ServletContext application = getServletContext();
+		List<Post> board = (List<Post>) application.getAttribute("board");
+		
+		String idStr = request.getParameter("id");
+		int id = Integer.parseInt(idStr);
+		Post post = board.get(id - 1);
+		
+		request.setAttribute("post", post);
+		
+		String path = "/WEB-INF/chapter16/view.jsp";
+		RequestDispatcher view = request.getRequestDispatcher(path);
+		view.forward(request, response);
+	
+		
+	
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
