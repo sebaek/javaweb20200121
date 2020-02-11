75e154d73db0e460429ef37a293dab47e497209d
choongangkangnam2 <choongangkangnam2@choongang.com>
2020-02-11 12:32:27 +0900

    add create connection pool

--- src/listener/AppListener.java
+++ src/listener/AppListener.java
@@ -1,5 +1,8 @@
 package listener;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,6 +12,8 @@ import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 
 import chapter16.Post;
+import oracle.ucp.jdbc.PoolDataSource;
+import oracle.ucp.jdbc.PoolDataSourceFactory;
 
 /**
  * Application Lifecycle Listener implementation class AppListener
@@ -17,40 +22,75 @@ import chapter16.Post;
 @WebListener
 public class AppListener implements ServletContextListener {
 
-    /**
-     * Default constructor. 
-     */
-    public AppListener() {
-        // TODO Auto-generated constructor stub
-    }
+	/**
+	 * Default constructor.
+	 */
+	public AppListener() {
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
-     * @see ServletContextListener#contextDestroyed(ServletContextEvent)
-     */
-    public void contextDestroyed(ServletContextEvent sce)  { 
-         // TODO Auto-generated method stub
-    }
+	 * @see ServletContextListener#contextDestroyed(ServletContextEvent)
+	 */
+	public void contextDestroyed(ServletContextEvent sce) {
+		// TODO Auto-generated method stub
+	}
 
 	/**
-     * @see ServletContextListener#contextInitialized(ServletContextEvent)
-     */
-    public void contextInitialized(ServletContextEvent sce)  {
-    	ServletContext application = sce.getServletContext();
-        application.setRequestCharacterEncoding("utf-8");
-        
-        List<Post> board = new ArrayList<>();
-        application.setAttribute("board", board);
-    }
-	
+	 * @see ServletContextListener#contextInitialized(ServletContextEvent)
+	 */
+	public void contextInitialized(ServletContextEvent sce) {
+		// set request character encoding to utf-8
+		ServletContext application = sce.getServletContext();
+		application.setRequestCharacterEncoding("utf-8");
+
+		// fake db
+		List<Post> board = new ArrayList<>();
+		application.setAttribute("board", board);
+
+		// real db connection
+		createDBCP();
+	}
+
+	private void createDBCP() {
+		try {
+			// Create pool-enabled data source instance.
+
+			PoolDataSource pds = PoolDataSourceFactory.getPoolDataSource();
+
+			// set the connection properties on the data source.
+
+			pds.setConnectionFactoryClassName("oracle.jdbc.pool.OracleDataSource");
+			pds.setURL("jdbc:oracle:thin:@//localhost:1521/XE");
+			pds.setUser("c##mydb");
+			pds.setPassword("admin");
+
+			// Override any pool properties.
+
+			pds.setInitialPoolSize(5);
+
+			// Get a database connection from the datasource.
+
+			Connection conn = pds.getConnection();
+
+			System.out.println("\nConnection obtained from " + "UniversalConnectionPool\n");
+
+			// do some work with the connection.
+			Statement stmt = conn.createStatement();
+			stmt.execute("select * from dual");
+
+			// Close the Connection.
+
+			conn.close();
+			conn = null;
+
+			System.out.println("Connection returned to the " + "UniversalConnectionPool\n");
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println("BasicConnectionExample - " + "main()-SQLException occurred : " + e.getMessage());
+		}
+
+	}
+
 }
-
-
-
-
-
-
-
-
-
-
-
