3410cb1f3049181d2f1173ddbe6fdf6b472f517b
choongangkangnam2 <choongangkangnam2@choongang.com>
2020-02-18 12:15:25 +0900

    add post

---
+++ WebContent/WEB-INF/chap14/insert2/post.jsp
@@ -0,0 +1,37 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<title>Insert title here</title>
+</head>
+<body>
+<h1>게시물 등록</h1>
+
+<div>
+	<form method="post">
+		<input name="title" placeholder="제목"><br>
+		<textarea name="body" rows="5" cols="20"></textarea><br>
+		<input type="submit" value="등록">
+	</form>
+</div>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/chapter14/insert2/BoardInsertServlet.java
+++ src/chapter14/insert2/BoardInsertServlet.java
@@ -9,22 +9,20 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
-import chapter14.bean.Member;
 import chapter14.util.DBCP;
 
 /**
- * Servlet implementation class SignUp2Servlet
+ * Servlet implementation class BoardInsertServlet
  */
-@WebServlet("/chapter14/signup")
-public class SignUp2Servlet extends HttpServlet {
+@WebServlet("/chapter14/board/post")
+public class BoardInsertServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public SignUp2Servlet() {
+    public BoardInsertServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -33,7 +31,7 @@ public class SignUp2Servlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String view = "/WEB-INF/chap14/insert2/signup.jsp";
+		String view = "/WEB-INF/chap14/insert2/post.jsp";
 		request.getRequestDispatcher(view).forward(request, response);
 	}
 
@@ -41,58 +39,40 @@ public class SignUp2Servlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// request 파라미터로 Member 빈 만들기
-		String id = request.getParameter("id");
-		String pw = request.getParameter("pw");
-		Member m = new Member();
-		m.setId(id);
-		m.setPassword(pw);
+		// request 파라미터 값 받고
+		String title = request.getParameter("title");
+		String body = request.getParameter("body");
 		
-		// Member빈으로 데이터 입력하기
-		boolean success = addMember(m);
+		// db에 insert
+		addPost(title, body);
 		
-		if (success) {
-			// - 입력성공하면 메인으로 redirection
-			HttpSession s = request.getSession();
-			s.setAttribute("user", m);
-			response.sendRedirect(request.getContextPath() 
-					+ "/chapter14/main");
-		} else {
-			// - 입력실패하면 signup.jsp로 forward
-			request.setAttribute("message", "가입실패");
-			doGet(request, response);
-		}
-		
+		// forward or redirect
+		doGet(request, response);
 	}
 
-	private boolean addMember(Member m) {
-		String sql = "INSERT INTO member (id, password) "
-				+ "VALUES (? , ? ) ";
+	private void addPost(String title, String body) {
+		String sql = "INSERT INTO board "
+				+ "(title, body) "
+				+ "VALUES (?, ?) ";
 		
 		try (
 			Connection con = DBCP.getConnection();
 			PreparedStatement stmt = con.prepareStatement(sql);
 		) {
-			stmt.setString(1, m.getId());
-			stmt.setString(2, m.getPassword());
-			int cnt = stmt.executeUpdate();
-			if (cnt < 1) {
-				return false;
-			}
+			stmt.setString(1, title);
+			stmt.setString(2, body);
+			stmt.executeQuery();
+			
 			
 		} catch (Exception e) {
 			e.printStackTrace();
-			return false;
 		}
-		
-		return true;
 	}
+	
+	
+	
 
 }
-
-
-
-
 
 
 
